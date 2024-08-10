5fce263a606c5c35f4b00898eb195028beb6b465
Ubuntu <ubuntu@ip-172-31-0-48.us-east-2.compute.internal>
2024-08-10 14:01:57 +0000

    Fixed trailing newline in __init__.py

--- src/pkg_tzs5dd/__init__.py
+++ src/pkg_tzs5dd/__init__.py
@@ -1,6 +1,8 @@
-# Import the functions from text_processor.py
+"""
+Initialization file for the pkg_tzs5dd package.
+This module imports and exposes the main functions for external use.
+"""
+
 from .text_processor import clean_text, tokenize, count_words
 
-# Define what is available when you import from the package
 __all__ = ["clean_text", "tokenize", "count_words"]
-
